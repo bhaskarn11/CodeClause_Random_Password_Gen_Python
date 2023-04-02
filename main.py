751235cf34644aabc5f8648a9537abc44bcfe23d
Bhaskar <bhaskarnar14@gmail.com>
2023-04-02 13:44:35 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import secrets
+import string
+
+
+def pass_gen(pass_len):
+
+    letters = string.ascii_letters
+    digits = string.digits
+    special_chars = string.punctuation
+
+    char_set = letters + digits + special_chars
+
+    while True:
+        pwd = ""
+        for i in range(pass_len):
+            pwd += pwd.join(secrets.choice(char_set))
+
+        if any(char in special_chars for char in pwd) and sum(char in char_set for char in pwd) >= 2:
+            return pwd
+
+
+if __name__ == "__main__":
+    n = int(input("Enter password length: "))
+    print("Generated password: " + pass_gen(n))
